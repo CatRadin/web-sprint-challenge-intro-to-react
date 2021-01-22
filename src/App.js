60ea1cef9fc64d767c0b6d9f92a570fb15baf276
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-01-22 17:19:21 -0600

    character

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characters, setCharacters] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonDetails, setPokemonDetails] = useState([]);
+  // const [characterData, setCharacterData] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+      setCharacters(response.data.results)
+      console.log(response.data.results)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  },[])
+  
+  // useEffect(() => {
+  //   axios.get('https://pokeapi.co/api/v2/pokemon/')
+  // .then(res => {
+  //   const fetches = res.data.results.map(p => axios.get(p.url))
+    
+  //   Promise.all(fetches).then(data => {
+  //     console.log(data);
+  //     setPokemon(data);
+  //   })
+  // });
+  // },[])
+  
+  // useEffect(() => {
+  //   promise = [];
+  //   for (i = 0; i < 20; i++) {
+  // promise.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`))
+  //   }
+  //   console.log(promise);
+  //   },[])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((characters, id) => (
+        <Character key ={id} character ={characters} />
+      ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,42 @@
 // Write your Character component here
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+export default function Character(props) {
+    const { character } = props;
+    const [showDetails, setShowDetails] = useState(false);
+
+    function onClick() {
+        setShowDetails((showDetails) => !showDetails)
+    }
+
+    return (
+
+<StyledCharacters>
+<div className= 'char-container'>
+<h2>{character.name}</h2>
+<p>Birth Year: {character.birth_year}</p>
+</div>
+
+</StyledCharacters>
+
+    )
+}
+
+const StyledCharacters = styled.div`
+color: white;
+display:flex;
+justify-content:center;
+align-items: center;
+
+h2 {
+    background: black;
+    border-radius: 1rem;
+    padding: 1rem;
+}
+
+p {
+    background: darkgrey;
+}
+
+`
